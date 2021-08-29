b30c118058b55ab3a94a470fc4b54418f520d74c
Denis Kotov <dkotov@transinform.info>
2021-08-30 01:48:40 +0300

    Change URL to http://www.transinform.info

--- checkwebpage.py
+++ checkwebpage.py
@@ -1,7 +1,8 @@
 import requests
 
 if __name__ == '__main__':
-    url = r"https://customers.rai.ir"
+    #url = r"https://customers.rai.ir"
+    url = r"http://www.transinform.info"
     session = requests.Session()
     content = session.get(url)
     print(content)
